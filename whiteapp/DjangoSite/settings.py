a20b6ef4733579a5cee277f27e04ae94626ee0f3
Adrien Ehrhardt <UT2WLS@EMEA.CIB>
2021-08-20 14:40:38 +0200

    domain name allowed hosts without *

--- whiteapp/DjangoSite/settings.py
+++ whiteapp/DjangoSite/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["localhost",
                  "127.0.0.1",
-                 "*.k8s.caas.ca-sa.gca"]
+                 ".k8s.caas.ca-sa.gca"]
 
 # Application definition
 
