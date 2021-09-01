8ddbb0834e9111fb1b69498812c02bc9990cc82b
DAMAY, Nicolas <nicolas.damay@ca-cib.com>
2021-09-01 18:35:04 +0200

    Attempt to fix mimetypes issue

--- whiteapp/DjangoSite/urls.py
+++ whiteapp/DjangoSite/urls.py
@@ -20,8 +20,11 @@ Examples:
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from WhiteApp.views import Accueil
+
 
 urlpatterns = [
     path('', Accueil.as_view(), name='Accueil'),
@@ -29,4 +32,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', auth_views.LoginView.as_view(template_name="login.html"), name="login"),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
