50a1245194cb8076a8c9853b752b4f31c2233794
Adrien Ehrhardt <UT2WLS@EMEA.CIB>
2021-10-20 16:44:03 +0200

    without debug, deleted pipeline backup, stored coverage as artifact

--- whiteapp/DjangoSite/settings.py
+++ whiteapp/DjangoSite/settings.py
@@ -28,7 +28,6 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', False)
-DEBUG = True
 
 ALLOWED_HOSTS = ["localhost",
                  "127.0.0.1",
